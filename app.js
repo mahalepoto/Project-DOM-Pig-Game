1d77d98331742e40902b8e51a865ee6e9dc9b22b
Mushtaq A Halepoto <mahalepoto@outlook.com>
2020-04-08 01:35:31 +0100

    Dice is working now.

--- app.js
+++ app.js
@@ -9,15 +9,47 @@ GAME RULES:
 
 */
 
-var dice, activePlayer;
-dice = 0;
-activePlayer = 0;
+let dice = 0;
+	activePlayer = 1;
+	scores = [0,0];
+	roundScore = 0;
 
   // Math.random gives you a random number in decimal numbers, but only in the
   // rang from 0 to 1.
   // Math.floor is the function that changes decimal numbers in whole numbers.
 
-dice = Math.floor(Math.random() *6) +1;
-console.log(dice); // for test only
 
-document.querySelector('#current-' + activePlayer).textContent = dice;
+ // console.log(dice);// for test only
+
+ // DOM manipulation
+	// document.querySelector('#current-' + activePlayer).textContent = dice;
+ // hiding the dice
+	document.querySelector('.dice').style.display = 'none';
+	document.getElementById('score-0').textContent = 0;
+	document.getElementById('score-1').textContent = 0;
+	document.getElementById('current-0').textContent = 0;
+	document.getElementById('current-1').textContent = 0;
+
+// function rollDice() {
+// 	dice = Math.floor(Math.random() *6) +1;
+// 	console.log(dice);
+// }
+
+	document.querySelector('.btn-roll').addEventListener('click', function() {
+		
+		dice = Math.floor(Math.random() *6) +1;
+		console.log(dice);
+
+		var diceRoll = document.querySelector('.dice')
+			diceRoll.style.display = 'block';
+			diceRoll.src = 'dice-' + dice + '.png';
+
+
+
+	});
+
+// // click event
+// document.querySelector('.btn-roll').addEventListener('click', function () {
+// 	dice = Math.floor(Math.random() *6) +1;
+// 	console.log(dice);
+// });
